3f6c83d5765073838886f2f4bd56c2127daef4cb
Nex Otaku <nex@otaku.ru>
2017-04-30 16:33:58 +0300

    Подключил ссылку на WinRT компонент в зависимости JS-проекта, добавил файлы для JS-заглушки.

--- QuestNavigatorStandaloneWindows10/js/main.js
+++ QuestNavigatorStandaloneWindows10/js/main.js
@@ -1,1 +1,35 @@
 ﻿// Место для кода.
+
+document.getElementById('logResult').innerHTML = 'log log';
+
+var helloPlugin = new QspLibWinRT.Hello();
+
+jQuery(function () {
+    $('#logResult').html('document is ready');
+
+    $(document).on('click', '#helloButtonGetMessage', function () {
+        HelloButtonGetMessage_Click();
+    });
+
+    $(document).on('click', '#helloButtonGetModifiedMessage', function () {
+        HelloButtonGetModifiedMessage_Click();
+    });
+
+    $(document).on('click', '#Button_Clear', function () {
+        ButtonClear_Click();
+    });
+});
+
+function ButtonClear_Click() {
+    document.getElementById('logResult').innerHTML = "";
+}
+
+function HelloButtonGetMessage_Click() {
+    var message = helloPlugin.getMessage();
+    $('#logResult').html(message);
+}
+
+function HelloButtonGetModifiedMessage_Click() {
+    var message = helloPlugin.getModifiedMessage("Эй, привет!");
+    $('#logResult').html(message);
+}
