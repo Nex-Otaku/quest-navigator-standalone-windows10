efd72bd2247a93b51f9e07da6761c0e80ec29f67
Nex Otaku <nex@otaku.ru>
2017-05-10 15:20:53 +0300

    Сделал заглушки для колбэков API.

--- QspLibWinRT/src/quest-navigator/face/standalone-win10/QspLib.cpp
+++ QspLibWinRT/src/quest-navigator/face/standalone-win10/QspLib.cpp
@@ -7,4 +7,82 @@ namespace QspLibWinRT
 	QspLib::QspLib()
 	{
 	}
+
+	// Колбэки из яваскрипта к функциям API плеера.
+
+	// По приведению типов int, bool, wchar_t
+	// см. таблицу
+	// https://docs.microsoft.com/en-us/cpp/cppcx/fundamental-types-c-cx
+	// По строкам и массивам см.
+	// https://docs.microsoft.com/en-us/cpp/cppcx/type-system-c-cx
+
+	void QspLib::restartGame()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::executeAction(int32 pos)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::selectObject(int32 pos)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::loadGame()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::saveGame()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::saveSlotSelected(int32 index, int32 mode)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::msgResult()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::errorResult()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::userMenuResult(int32 pos)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::inputResult(Platform::String ^ text)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::setMute(Platform::Boolean flag)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::setInputString(Platform::String ^ text)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::runInputString()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void QspLib::runDefaultGame()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
 }
--- QspLibWinRT/src/quest-navigator/face/standalone-win10/QspLib.h
+++ QspLibWinRT/src/quest-navigator/face/standalone-win10/QspLib.h
@@ -6,5 +6,20 @@ namespace QspLibWinRT
 	{
 	public:
 		QspLib();
+
+		void restartGame();
+		void executeAction(int32 pos);
+		void selectObject(int32 pos);
+		void loadGame();
+		void saveGame();
+		void saveSlotSelected(int32 index, int32 mode);
+		void msgResult();
+		void errorResult();
+		void userMenuResult(int32 pos);
+		void inputResult(Platform::String^ text);
+		void setMute(Platform::Boolean flag);
+		void setInputString(Platform::String^ text);
+		void runInputString();
+		void runDefaultGame();
 	};
 }
