877b0feee2b2d9180e080f620fb70f1e3ef8355e
Nex Otaku <nex@otaku.ru>
2017-05-19 20:55:15 +0300

    Убрал лишнее, обновил фильтры.

--- QspLibWinRT/src/quest-navigator/face/standalone-win10/LibraryListener.h
+++ QspLibWinRT/src/quest-navigator/face/standalone-win10/LibraryListener.h
@@ -75,31 +75,4 @@ namespace QuestNavigator
 		Timer* timer;
 		EventManager* eventManager;
 	};
-	//		// ********************************************************************
-	//		// ********************************************************************
-	//		// ********************************************************************
-	//		//                       Колбэки интерпретатора
-	//		// ********************************************************************
-	//		// ********************************************************************
-	//		// ********************************************************************
-	//
-	//		static void RefreshInt(int isRedraw);
-	//		static void SetTimer(int msecs);
-	//		static void SetInputStrText(QSP_CHAR* text);
-	//		static void ShowMessage(QSP_CHAR* message);
-	//		static void PlayFile(QSP_CHAR* file, int volume);
-	//		static QSP_BOOL IsPlayingFile(QSP_CHAR* file);
-	//		static void CloseFile(QSP_CHAR* file);
-	//		static void ShowPicture(QSP_CHAR* file);
-	//		static void InputBox(const QSP_CHAR* prompt, QSP_CHAR* buffer, int maxLen);
-	//		static void PlayerInfo(QSP_CHAR* resource, QSP_CHAR* buffer, int maxLen);
-	//		static int GetMSCount();
-	//		static void AddMenuItem(QSP_CHAR* name, QSP_CHAR* imgPath);
-	//		static int ShowMenu();
-	//		static void DeleteMenu();
-	//		static void Wait(int msecs);
-	//		static void ShowWindow(int type, QSP_BOOL isShow);
-	//		static void System(QSP_CHAR* cmd);
-	//		static void OpenGameStatus(QSP_CHAR* file);
-	//		static void SaveGameStatus(QSP_CHAR* file);
 }
