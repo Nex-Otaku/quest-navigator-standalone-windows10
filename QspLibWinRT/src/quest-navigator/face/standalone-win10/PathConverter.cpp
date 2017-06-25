cce55869b2104f7821a23f23fa13c7dcfc96b29f
Nex Otaku <nex@otaku.ru>
2017-06-25 15:24:01 +0300

    Сделал преобразование пути из абсолютного в относительный.

--- QspLibWinRT/src/quest-navigator/face/standalone-win10/PathConverter.cpp
+++ QspLibWinRT/src/quest-navigator/face/standalone-win10/PathConverter.cpp
@@ -2,6 +2,8 @@
 #include "PathConverter.h"
 #include <string>
 #include "..\..\platform\windows10\ApplicationPathReader.h"
+#include "..\..\core\dialogs.h"
+#include "..\..\core\strings.h"
 
 using namespace std;
 
@@ -24,7 +26,22 @@ namespace QuestNavigator {
 	// Нужно получить: "../standalone_content/skins/gfx/my-image.png"
 	string PathConverter::convertAbsolutePathToRelative(string path)
 	{
-		// STUB
-		return "";
+		// "D:\QuestNavigator"
+		string rootAppPath = applicationPathReader->getApplicationFolderPath();
+		// "D:\QuestNavigator\game\"
+		string gamePath = rootAppPath + "\\game\\";
+		if (!startsWith(path, gamePath)) {
+			showError("Не удалось преобразовать путь к файлу.");
+			return path;
+		}
+
+		size_t length = gamePath.size();
+		// "standalone_content\skins\gfx\my-image.png"
+		string tail = path.substr(length);
+
+		// "../standalone_content/skins/gfx/my-image.png"
+		string relativePath = "../" + backslashToSlash(tail);
+		
+		return relativePath;
 	}
 }
