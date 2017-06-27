e4cf2ebb1b57fa0cc3b65700e917984de58fd794
Nex Otaku <nex@otaku.ru>
2017-06-27 11:30:49 +0300

    Убрал лишнее.

--- QspLibWinRT/src/quest-navigator/face/standalone-win10/SaveFileManager.cpp
+++ QspLibWinRT/src/quest-navigator/face/standalone-win10/SaveFileManager.cpp
@@ -84,24 +84,6 @@ namespace QuestNavigator {
 
 	string SaveFileManager::getFullSavePath(string relativePath)
 	{
-
-		//// "D:\MySaveDirForCoolGame"
-		//string saveDir = Configuration::getString(ecpSaveDir);
-		//// "D:\CoolGame\saves\save1.sav"
-		//string originalSaveFilePath = getRightPath(file);
-		//// "D:\CoolGame\game.qsp"
-		//string gameFilePath = getRightPath(Configuration::getString(ecpGameFilePath));
-		//// "game.qsp"
-		//string gameFileName = Configuration::getString(ecpGameFileName);
-		//// "D:\CoolGame\"
-		//string gameDir = gameFilePath.substr(0, gameFilePath.length() - gameFileName.length());
-		//// "saves\save1.sav"
-		//string relativeSavePath = originalSaveFilePath.substr(gameDir.length());
-		//// "D:\MySaveDirForCoolGame\saves\save1.sav"
-		//string saveFile = getRightPath(saveDir + PATH_DELIMITER + relativeSavePath);
-		//return saveFile;
-
-
 		string saveDir = Configuration::getString(ecpSaveDir);
 		string path = getRightPath(saveDir + PATH_DELIMITER + relativePath);
 		return path;
